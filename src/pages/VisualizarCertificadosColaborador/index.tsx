121b451c22cc21fcb03e9740b9aab54df9eec8a6
LucasLessa <lessa07oliveira@gmail.com>
2023-09-11 20:18:04 -0300

    l

--- src/pages/VisualizarCertificadosColaborador/index.tsx
+++ src/pages/VisualizarCertificadosColaborador/index.tsx
@@ -2,7 +2,7 @@
 import Aside from "../../components/aside";
 
 import "./style.css";
-
+import CertificadosIcon from "../../assets/icons/certificado2.png"
 
 function VisualizarCertificadosColaborador() {
     return (
@@ -10,24 +10,52 @@ function VisualizarCertificadosColaborador() {
 
         <main id="aside_colaborador">
             <Aside />
-            <div className="MeusCertificados">
+            {/* <div className="MeusCertificados">
                 <div className="Caixas">
-                <div className="caixaCertificado1">
-                </div>
-                <div className="caixaCertificado2">
+                    <div className="caixaCertificado1">
+                    </div>
+                    <div className="caixaCertificado2">
+                    </div>
+
+                    <div className="caixaCertificado3">
+                    </div>
                 </div>
 
-                <div className="caixaCertificado3">
+
+
+            </div> */}
+
+
+            <div className="elementos_carrousel">
+                <div className="carrousel_texto">
+                    <h2>Certificados</h2>
+                    <p>Acesse os certificados que foi adquirindo em sua jornada:</p>
                 </div>
+                <div className="carrousel_certificado">
+                    {/* <img src="assets/icons/flecha_esquerda.svg" alt="icone flecha" /> */}
+                    <div className="certificados1">
+                        <img src={CertificadosIcon} alt="certificado" />
+                        <img src={CertificadosIcon} alt="certificado" />
+                        <img src={CertificadosIcon} alt="certificado" />
+                    </div>
+                    <div className="certificados2">
+                        <img src={CertificadosIcon} alt="certificado" />
+                        <img src={CertificadosIcon} alt="certificado" />
+                        <img src={CertificadosIcon} alt="certificado" />
+                       </div>
+                        {/* // className="flecha_esquerda"
+                        // src="assets/icons/flecha_direita.svg"
+                        // alt="icone flecha"
+                        /> */}
+                    </div>
                 </div>
 
-
-
-            </div>
         </main>
 
     )
 }
+
+
 export default VisualizarCertificadosColaborador
 
 
